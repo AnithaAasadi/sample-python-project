1a14ccb1e94dd815025c8dfe4f71d51df9da87e3
Anitha  Aasadi <87598908+AnithaAasadi@users.noreply.github.com>
2025-04-04 19:03:32 +0530

    changed

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull anitha827/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 anitha827/simple-python-flask-app #-d demon running backgroud 
